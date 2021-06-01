0f631a7801c68805ebe4d65eae76514e8bf4c617
shuangjian.zhou <zhoushuangjian@blhcn.com>
2021-06-01 17:12:09 +0800

    优化

--- lib/src/ui/idkit_screen.dart
+++ lib/src/ui/idkit_screen.dart
@@ -1,41 +1,150 @@
-import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
-import 'package:flutter_idkit/src/common/idkit_tap.dart';
-import 'package:flutter_idkit/src/function/idkit_screen_adapter.dart';
 
-class IDKitScreen extends StatelessWidget {
-  const IDKitScreen({
+class MaterialScreenApp extends MaterialApp {
+  const MaterialScreenApp({
     Key? key,
-    required this.builder,
-    this.designSize,
-    this.isFollow,
-  })  : assert(builder != null),
-        super(key: key);
+    GlobalKey<NavigatorState>? navigatorKey,
+    GlobalKey<ScaffoldMessengerState>? scaffoldMessengerKey,
+    Widget? home,
+    Map<String, WidgetBuilder> routes = const <String, WidgetBuilder>{},
+    String? initialRoute,
+    RouteFactory? onGenerateRoute,
+    InitialRouteListFactory? onGenerateInitialRoutes,
+    RouteFactory? onUnknownRoute,
+    List<NavigatorObserver> navigatorObservers = const <NavigatorObserver>[],
+    TransitionBuilder? builder,
+    String title = '',
+    GenerateAppTitle? onGenerateTitle,
+    ThemeData? theme,
+    ThemeData? darkTheme,
+    ThemeData? highContrastTheme,
+    ThemeData? highContrastDarkTheme,
+    ThemeMode? themeMode,
+    Color? color,
+    Locale? locale,
+    Iterable<LocalizationsDelegate<dynamic>>? localizationsDelegates,
+    LocaleListResolutionCallback? localeListResolutionCallback,
+    LocaleResolutionCallback? localeResolutionCallback,
+    Iterable<Locale> supportedLocales = const <Locale>[Locale('en', 'US')],
+    bool showPerformanceOverlay = false,
+    bool debugShowMaterialGrid = false,
+    bool checkerboardRasterCacheImages = false,
+    bool checkerboardOffscreenLayers = false,
+    bool showSemanticsDebugger = false,
+    bool debugShowCheckedModeBanner = true,
+    Map<LogicalKeySet, Intent>? shortcuts,
+    Map<Type, Action<Intent>>? actions,
+    String? restorationScopeId,
+    this.isFollow = true,
+  }) : super(
+          key: key,
+          navigatorKey: navigatorKey,
+          scaffoldMessengerKey: scaffoldMessengerKey,
+          home: home,
+          routes: routes,
+          initialRoute: initialRoute,
+          onGenerateRoute: onGenerateRoute,
+          onGenerateInitialRoutes: onGenerateInitialRoutes,
+          onUnknownRoute: onUnknownRoute,
+          navigatorObservers: navigatorObservers,
+          title: title,
+          builder: builder,
+          onGenerateTitle: onGenerateTitle,
+          theme: theme,
+          darkTheme: darkTheme,
+          highContrastTheme: highContrastTheme,
+          highContrastDarkTheme: highContrastDarkTheme,
+          themeMode: themeMode,
+          color: color,
+          locale: locale,
+          localizationsDelegates: localizationsDelegates,
+          localeListResolutionCallback: localeListResolutionCallback,
+          localeResolutionCallback: localeResolutionCallback,
+          supportedLocales: supportedLocales,
+          showPerformanceOverlay: showPerformanceOverlay,
+          debugShowMaterialGrid: debugShowMaterialGrid,
+          checkerboardRasterCacheImages: checkerboardRasterCacheImages,
+          checkerboardOffscreenLayers: checkerboardOffscreenLayers,
+          showSemanticsDebugger: showSemanticsDebugger,
+          debugShowCheckedModeBanner: debugShowCheckedModeBanner,
+          shortcuts: shortcuts,
+          actions: actions,
+          restorationScopeId: restorationScopeId,
+        );
 
-  final IDKitScreenBuilder builder;
+  final bool isFollow;
 
-  final Size? designSize;
-
-  final bool? isFollow;
+  const MaterialScreenApp.router({
+    Key? key,
+    GlobalKey<ScaffoldMessengerState>? scaffoldMessengerKey,
+    RouteInformationProvider? routeInformationProvider,
+    required RouteInformationParser<Object> routeInformationParser,
+    required RouterDelegate<Object> routerDelegate,
+    BackButtonDispatcher? backButtonDispatcher,
+    TransitionBuilder? builder,
+    String title = '',
+    GenerateAppTitle? onGenerateTitle,
+    ThemeMode? themeMode,
+    Color? color,
+    Locale? locale,
+    ThemeData? theme,
+    ThemeData? darkTheme,
+    ThemeData? highContrastTheme,
+    ThemeData? highContrastDarkTheme,
+    Iterable<LocalizationsDelegate<dynamic>>? localizationsDelegates,
+    LocaleListResolutionCallback? localeListResolutionCallback,
+    LocaleResolutionCallback? localeResolutionCallback,
+    Iterable<Locale> supportedLocales = const <Locale>[Locale('en', 'US')],
+    bool showPerformanceOverlay = false,
+    bool debugShowMaterialGrid = false,
+    bool checkerboardRasterCacheImages = false,
+    bool checkerboardOffscreenLayers = false,
+    bool showSemanticsDebugger = false,
+    bool debugShowCheckedModeBanner = true,
+    Map<LogicalKeySet, Intent>? shortcuts,
+    Map<Type, Action<Intent>>? actions,
+    String? restorationScopeId,
+    this.isFollow = true,
+  }) : super(
+          key: key,
+          scaffoldMessengerKey: scaffoldMessengerKey,
+          title: title,
+          onGenerateTitle: onGenerateTitle,
+          theme: theme,
+          darkTheme: darkTheme,
+          highContrastTheme: highContrastTheme,
+          highContrastDarkTheme: highContrastDarkTheme,
+          themeMode: themeMode,
+          builder: builder,
+          color: color,
+          locale: locale,
+          localizationsDelegates: localizationsDelegates,
+          localeListResolutionCallback: localeListResolutionCallback,
+          localeResolutionCallback: localeResolutionCallback,
+          supportedLocales: supportedLocales,
+          showPerformanceOverlay: showPerformanceOverlay,
+          debugShowMaterialGrid: debugShowMaterialGrid,
+          checkerboardRasterCacheImages: checkerboardRasterCacheImages,
+          checkerboardOffscreenLayers: checkerboardOffscreenLayers,
+          showSemanticsDebugger: showSemanticsDebugger,
+          debugShowCheckedModeBanner: debugShowCheckedModeBanner,
+          shortcuts: shortcuts,
+          actions: actions,
+          restorationScopeId: restorationScopeId,
+        );
 
   @override
-  Widget build(BuildContext context) {
-    return MediaQuery(
-        data: MediaQueryData.fromWindow(
-                WidgetsBinding.instance?.window ?? ui.window)
-            .copyWith(textScaleFactor: 1.0),
-        child: LayoutBuilder(builder: (_, BoxConstraints constraints) {
-          return OrientationBuilder(builder: (_, Orientation orientation) {
-            if (constraints.maxHeight != 0 || constraints.maxWidth != 0) {
-              IDKitScreenAdapter.build(
-                constraints,
-                orientation,
-                designSize: designSize,
-              );
-              return builder.call();
-            }
-            throw 'The upper transitive constraint [maxHeight] and [maxWidth] is zero.';
-          });
-        }));
-  }
+  TransitionBuilder? get builder => (BuildContext context, Widget? child) {
+        if (isFollow) {
+          return child!;
+        } else {
+          return MediaQuery(
+              data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+              child: child!);
+        }
+      };
+
+  @override
+  // TODO: implement routerDelegate
+  RouterDelegate<Object>? get routerDelegate => super.routerDelegate;
 }
