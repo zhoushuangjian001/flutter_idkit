22af9dbacb0ee228cf7741ed57551150197b5853
shuangjian.zhou <zhoushuangjian@blhcn.com>
2021-06-01 14:32:36 +0800

    优化

--- lib/src/function/idkit_screen_adapter.dart
+++ lib/src/function/idkit_screen_adapter.dart
@@ -15,64 +15,64 @@ class IDKitScreenAdapter {
     Orientation orientation, {
     Size? designSize,
   }) {
-    _instance = IDKitScreenAdapter._()
-      .._orientation = orientation
-      .._screenHeight = constraints.maxHeight
-      .._screenWidth = constraints.maxWidth;
+    _instance = IDKitScreenAdapter._();
+    _orientation = orientation;
+    _screenHeight = constraints.maxHeight;
+    _screenWidth = constraints.maxWidth;
     final Size _size = designSize ??= defaultUISize;
-    _instance._uiSize =
-        orientation == Orientation.portrait ? _size : _size.flipped;
+    _uiSize = orientation == Orientation.portrait ? _size : _size.flipped;
     // The configure of device.
     final ui.SingletonFlutterWindow window =
         WidgetsBinding.instance?.window ?? ui.window;
-    _instance
-      .._devicePixelRatio = window.devicePixelRatio
-      .._textScaleFactor = window.textScaleFactor
-      .._statusBarHeight = window.padding.top
-      .._bottomBarHeight = window.padding.bottom;
+    _devicePixelRatio = window.devicePixelRatio;
+    _textScaleFactor = window.textScaleFactor;
+    _statusBarHeight = window.padding.top;
+    _bottomBarHeight = window.padding.bottom;
   }
 
   /// the orientation of the screen.
-  late Orientation _orientation;
-  late double _devicePixelRatio;
-  late double _textScaleFactor;
-  late double _screenWidth;
-  late double _screenHeight;
-  late double _statusBarHeight;
-  late double _bottomBarHeight;
-  late Size _uiSize;
+  static late Orientation _orientation;
+  static late double _devicePixelRatio;
+  static late double _textScaleFactor;
+  static late double _screenWidth;
+  static late double _screenHeight;
+  static late double _statusBarHeight;
+  static late double _bottomBarHeight;
+  static late Size _uiSize;
 
   /// Get the orientation of the screen.
-  Orientation get orientation => _orientation;
+  static Orientation get orientation => _orientation;
+  static double get devicePixelRatio => _devicePixelRatio;
 
   /// Get the width of the screen.
-  double get screenWidth => _screenWidth;
+  static double get screenWidth => _screenWidth;
 
   /// Get the height of the screen.
-  double get screenHeight => _screenHeight;
+  static double get screenHeight => _screenHeight;
 
-  double get statusBarHeight => _statusBarHeight;
+  static double get statusBarHeight => _statusBarHeight;
 
-  double get bottomBarHeight => _bottomBarHeight;
+  static double get bottomBarHeight => _bottomBarHeight;
 
   /// Get the ratio of the screen width to the design width.
-  double get scaleWidth => _screenWidth / _uiSize.width;
+  static double get scaleWidth => _screenWidth / _uiSize.width;
 
   /// Get the ratio of the screen height to the design height.
-  double get scaleHeight => _screenHeight / _uiSize.height;
+  static double get scaleHeight => _screenHeight / _uiSize.height;
 
   /// Get the ratio of the text font to the design text font.
-  double get scaleFont => min(scaleWidth, scaleHeight);
+  static double get scaleFont => min(scaleWidth, scaleHeight);
 
   /// Get the mapping value of design width through width ratio.
-  double setWidth(num width) => scaleWidth * width;
+  static double setWidth(num width) => scaleWidth * width;
 
   /// Get the mapping value of design height through height ratio.
-  double setHeight(num height) => scaleHeight * height;
+  static double setHeight(num height) => scaleHeight * height;
 
   /// Get the mapping value of design text font through text font ratio.
   /// [allowFontScaling] is following system font scaling.
-  double setSp(num fontSize, {bool allowFontScaling = true}) => allowFontScaling
-      ? fontSize * scaleFont * _textScaleFactor
-      : fontSize * scaleFont;
+  static double setSp(num fontSize, {bool allowFontScaling = true}) =>
+      allowFontScaling
+          ? fontSize * scaleFont * _textScaleFactor
+          : fontSize * scaleFont;
 }
--- lib/src/function/idkit_screen_extension.dart
+++ lib/src/function/idkit_screen_extension.dart
@@ -1,18 +1,18 @@
 import 'package:flutter_idkit/src/function/idkit_screen_adapter.dart';
 
 extension IDKitScreenNum on num {
-  double get w => IDKitScreenAdapter().setWidth(this);
-  double get h => IDKitScreenAdapter().setHeight(this);
+  double get w => IDKitScreenAdapter.setWidth(this);
+  double get h => IDKitScreenAdapter.setHeight(this);
 
   /// 不小于
-  double get cw => IDKitScreenAdapter().setWidth(this).ceilToDouble();
-  double get ch => IDKitScreenAdapter().setHeight(this).ceilToDouble();
+  double get cw => IDKitScreenAdapter.setWidth(this).ceilToDouble();
+  double get ch => IDKitScreenAdapter.setHeight(this).ceilToDouble();
 
   /// 不大于
-  double get fw => IDKitScreenAdapter().setWidth(this).floorToDouble();
-  double get fh => IDKitScreenAdapter().setHeight(this).floorToDouble();
+  double get fw => IDKitScreenAdapter.setWidth(this).floorToDouble();
+  double get fh => IDKitScreenAdapter.setHeight(this).floorToDouble();
 
   /// 最接近
-  double get rw => IDKitScreenAdapter().setWidth(this).roundToDouble();
-  double get rh => IDKitScreenAdapter().setHeight(this).roundToDouble();
+  double get rw => IDKitScreenAdapter.setWidth(this).roundToDouble();
+  double get rh => IDKitScreenAdapter.setHeight(this).roundToDouble();
 }
